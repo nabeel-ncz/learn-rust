95337388b6ff8b00af734a5dcad109bd823c2cd6
nabeel-ncz <nabeelnabhan115@gmail.com>
2024-05-18 19:43:22 +0530

    initial setup & hello world

---
+++ Hello_World/target/debug/deps/Test_1-8afd1d686c2284bb.d
@@ -0,0 +1,5 @@
+C:\Users\user\OneDrive\Desktop\design-planner\exp-rust\Test-1\target\debug\deps\libTest_1-8afd1d686c2284bb.rmeta: src\main.rs
+
+C:\Users\user\OneDrive\Desktop\design-planner\exp-rust\Test-1\target\debug\deps\Test_1-8afd1d686c2284bb.d: src\main.rs
+
+src\main.rs:
