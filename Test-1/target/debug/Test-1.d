caf1f87e6f314bc5201850d9a5e83c42bcfb0112
nabeel-ncz <nabeelnabhan115@gmail.com>
2024-05-18 18:25:04 +0530

    hello world

---
+++ Test-1/target/debug/Test-1.d
@@ -0,0 +1,1 @@
+C:\Users\user\OneDrive\Desktop\design-planner\exp-rust\Test-1\target\debug\Test-1.exe: C:\Users\user\OneDrive\Desktop\design-planner\exp-rust\Test-1\src\main.rs
---
+++ Test-1/target/release/deps/Test_1.d
@@ -0,0 +1,5 @@
+C:\Users\user\OneDrive\Desktop\design-planner\exp-rust\Test-1\target\release\deps\Test_1.exe: src\main.rs
+
+C:\Users\user\OneDrive\Desktop\design-planner\exp-rust\Test-1\target\release\deps\Test_1.d: src\main.rs
+
+src\main.rs:
