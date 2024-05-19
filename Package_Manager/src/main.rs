ed78b3ccc709a41d6f18f2c61fef62fec183d503
nabeel-ncz <nabeelnabhan115@gmail.com>
2024-05-19 15:16:50 +0530

    basics of package manager

---
+++ Package_Manager/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("Hello, world!");
+
+    // some of the most popular packages using rust
+
+    // Actix - extremely fast http server
+    // Serde - serializing and deserializing data in rust
+    // Tokio - asyncronous runtime in rust
+    // Reqwest - to sent http request
+    // Sqlx - connect to sql database
+
+    // cargo init
+    // cargo run
+    // cargo build
+    // cargo add <package-name>
+    
+}
